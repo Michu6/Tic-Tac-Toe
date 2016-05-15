bbc5570773c006a231e27f51eba9871467e49940
Michu6 <karenmvaldivia@gmail.com>
2016-05-14 20:44:50 -0500

    casi listo el michi :3

--- js/main.js
+++ js/main.js
@@ -1,21 +1,56 @@
 $(document).ready(function() {
+	var uno = $('#uno').text();
+	var dos = $('#dos').text();
+	var tres = $('#tres').text();
+	var cuatro = $('#cuatro').text();
+	var cinco = $('#cinco').text();
+	var seis = $('#seis').text();
+	var siete = $('#siete').text();
+	var ocho = $('#ocho').text();
+	var nueve = $('#nueve').text();
+
+	var squares = [$('#uno'), $('#dos'), $('#tres'), $('#cuatro'), $('#cinco'), $('#seis'), $('#siete'), $('#ocho'), $('#nueve')];
+
 	var contador = 0;
 	$('.btnmichi').on('click', function(e) {
-		contador++
+		contador++;
 		if(contador%2===0) {
-			$(this).addClass('textred').html("O");
+			$(this).addClass('textred').html("O").attr('disabled', true);
 		} else {
-			$(this).html("X");
+			$(this).addClass('textred').html("X").attr('disabled', true);
 		}
-		//else {
-		// 		if($(this).hasClass('textred')) {
-	 //    		$(this).removeClass('textred');
-		// 		};
-		// 	$(this).html("O");
-		// }
 
-		
-
+		if(contador>4) {
+			if(uno != ' ' || dos != '' || tres != ' ' || cuatro != ' ' || cinco != ' ' || seis != ' ' || siete != ' ' || ocho != ' ' || nueve != ' '){
+				console.log(uno,dos,tres);
+				if(uno===dos && uno===tres && dos===tres) {
+					alert('PRIMERA FILA');
+				} else if(cuatro===cinco && cuatro===seis && cinco===seis) {
+					alert('SEGUNDA FILA');
+				} else if(siete===ocho && siete===nueve && ocho===nueve) {
+					alert('TERCERA FILA');
+				} else if(uno===cuatro && uno===siete && cuatro===siete) {
+					alert('PRIMERA COLUMNA');
+				} else if(dos===cinco && dos===siete && cinco===siete) {
+					alert('SEGUNDA COLUMNA');
+				} else if(tres===seis && tres===nueve && seis===nueve) {
+					alert('TERCERA COLUMNA');
+				} else if(uno===cinco && uno===nueve && cinco===nueve) {
+					alert('DIAGONAL 111111111111');
+				} else if(tres===cinco && tres===siete && cinco===siete) {
+					alert('DIAGONAL 222222222222');
+				}
+			}
+		}
 	});
+
+	$('#newgame').on('click', function () {
+		contador = 0;
+		squares.forEach(function (square) {
+			square
+				.text('+')
+				.removeClass('textred');
+		})
+	})
 	
 });
