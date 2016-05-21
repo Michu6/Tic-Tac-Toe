180b9d1266c6a28207fcc21ef57c22a10b476223
Michu6 <karenmvaldivia@gmail.com>
2016-05-20 20:16:11 -0500

    juego michiiii

--- js/main.js
+++ js/main.js
@@ -1,5 +1,19 @@
+function allTheSameAndNotEmpty(e1, e2, e3) {
+  return e1 === e2 && e1 === e3 && e1 !== '+';
+}
+
 $(document).ready(function() {
-	var uno = $('#uno').text();
+	var squares = [$('#uno'), $('#dos'), $('#tres'), $('#cuatro'), $('#cinco'), $('#seis'), $('#siete'), $('#ocho'), $('#nueve')];
+	var contador = 0;
+
+	$('.btnmichi').on('click', function(e) {
+		contador++;
+		if(contador%2===0) {
+			$(this).addClass('textred').html("O");
+		} else {
+			$(this).addClass('textred').html("X");
+		}
+  var uno = $('#uno').text();
 	var dos = $('#dos').text();
 	var tres = $('#tres').text();
 	var cuatro = $('#cuatro').text();
@@ -8,38 +22,23 @@ $(document).ready(function() {
 	var siete = $('#siete').text();
 	var ocho = $('#ocho').text();
 	var nueve = $('#nueve').text();
-
-	var squares = [$('#uno'), $('#dos'), $('#tres'), $('#cuatro'), $('#cinco'), $('#seis'), $('#siete'), $('#ocho'), $('#nueve')];
-
-	var contador = 0;
-	$('.btnmichi').on('click', function(e) {
-		contador++;
-		if(contador%2===0) {
-			$(this).addClass('textred').html("O").attr('disabled', true);
-		} else {
-			$(this).addClass('textred').html("X").attr('disabled', true);
-		}
-
-		if(contador>4) {
-			if(uno != ' ' || dos != '' || tres != ' ' || cuatro != ' ' || cinco != ' ' || seis != ' ' || siete != ' ' || ocho != ' ' || nueve != ' '){
-				console.log(uno,dos,tres);
-				if(uno===dos && uno===tres && dos===tres) {
-					alert('PRIMERA FILA');
-				} else if(cuatro===cinco && cuatro===seis && cinco===seis) {
-					alert('SEGUNDA FILA');
-				} else if(siete===ocho && siete===nueve && ocho===nueve) {
-					alert('TERCERA FILA');
-				} else if(uno===cuatro && uno===siete && cuatro===siete) {
-					alert('PRIMERA COLUMNA');
-				} else if(dos===cinco && dos===siete && cinco===siete) {
-					alert('SEGUNDA COLUMNA');
-				} else if(tres===seis && tres===nueve && seis===nueve) {
-					alert('TERCERA COLUMNA');
-				} else if(uno===cinco && uno===nueve && cinco===nueve) {
-					alert('DIAGONAL 111111111111');
-				} else if(tres===cinco && tres===siete && cinco===siete) {
-					alert('DIAGONAL 222222222222');
-				}
+		if(contador > 4 && contador < 10) {
+			if(allTheSameAndNotEmpty(uno, dos, tres)) {
+				alert('GANASTEEEEE EN PRIMERA FILA');
+			} else if(allTheSameAndNotEmpty(cuatro, cinco, seis)) {
+				alert('GANASTEEEEE EN SEGUNDA FILA');
+			} else if(allTheSameAndNotEmpty(siete, ocho, nueve)) {
+				alert('GANASTEEEEE EN TERCERA FILA');
+			} else if(allTheSameAndNotEmpty(uno, cuatro, siete)) {
+				alert('GANASTEEEEE EN PRIMERA COLUMNA');
+			} else if(allTheSameAndNotEmpty(dos, cinco, siete)) {
+				alert('GANASTEEEEE EN SEGUNDA COLUMNA');
+			} else if(allTheSameAndNotEmpty(tres, seis, nueve)) {
+				alert('GANASTEEEEE EN TERCERA COLUMNA');
+			} else if(allTheSameAndNotEmpty(uno, cinco, nueve)) {
+				alert('GANASTEEEEE EN DIAGONAL 111111111111');
+			} else if(allTheSameAndNotEmpty(tres, cinco, siete)) {
+				alert('GANASTEEEEE EN DIAGONAL 222222222222');
 			}
 		}
 	});
@@ -52,5 +51,4 @@ $(document).ready(function() {
 				.removeClass('textred');
 		})
 	})
-	
 });
